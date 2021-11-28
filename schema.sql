33f73cbd0f32a875f3b61cc511056f369d1266fb
kathynwe <ktnwe22@gmail.com>
2021-11-28 15:45:51 -0800

    updates

--- schema.sql
+++ schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
-	hire_date DATE NOT NULL, 
+	to_date DATE NOT NULL, 
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
@@ -41,17 +41,12 @@ CREATE TABLE titles (
     emp_no INT NOT NULL,
 	titles VARCHAR NOT NULL,
     from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+    to_date DATE NOT NULL
 );
 
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
     from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no)
+    to_date DATE NOT NULL
 );
