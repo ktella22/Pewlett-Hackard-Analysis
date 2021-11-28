e4cdad7c9ab4edfcb5d880fac8b2d278c996096b
kathynwe <ktnwe22@gmail.com>
2021-11-28 09:56:14 -0800

    update

--- schema.sql
+++ schema.sql
@@ -36,3 +36,22 @@ CREATE TABLE salaries (
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+	titles VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no)
+);
